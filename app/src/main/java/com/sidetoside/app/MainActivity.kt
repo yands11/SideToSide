97acbc0043010dbe7bdf94d5fe93f58472dd88bd
dot2line <yands11@gmail.com>
2019-12-10 00:21:56 +0900

    init

---
+++ app/src/main/java/com/sidetoside/app/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.sidetoside.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.setupWithNavController(
+            findNavController(R.id.nav_host_fragment).apply {
+                setupActionBarWithNavController(
+                    this,
+                    AppBarConfiguration(
+                        setOf(
+                            R.id.navigation_home,
+                            R.id.navigation_dashboard,
+                            R.id.navigation_notifications
+                        )
+                    )
+                )
+            })
+    }
+}
+
---
+++ app/src/main/java/com/sidetoside/app/ui/home/FeedAdapter.kt
@@ -0,0 +1,16 @@
+package com.sidetoside.app.ui.home
+
+import androidx.recyclerview.widget.RecyclerView
+
+
+class FeedAdapter : RecyclerView.Adapter<FeedAdapter.FeedHolder>() {
+
+
+    class FeedHolder(
+        val binding: HolderFeedBinding
+        val
+    ) : RecyclerView.ViewHolder(root) {
+
+    }
+}
+
